c6a1a5d0dbb956e8ff2f90928ff3b0aad30e7244
Paulo <paulo_marques_mesquita@hotmail.com>
2023-12-29 17:28:11 -0300

    Meals Project

---
+++ lib/components/main_drawer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Text('Vamos Cozinhar'),
+          ),
+        ],
+      ),
+    );
+  }
+}
